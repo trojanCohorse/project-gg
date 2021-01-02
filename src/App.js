0e96ae0c57d258a642a4d6519def50461897ecaf
Asif <asif14.k@hotmail.com>
2021-01-02 16:27:00 -0500

    Added oauth package

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 import './sass/App.scss';
 import InputReferences from './components/InputReferences';
 
 const App = () => {
   return (
+    <Auth0Provider
+    domain="codedkhan.us.auth0.com"
+    clientId="1fAqEsZEmLK7Lfrh0PP01H8YbIPCs1q0"
+    redirectUri={window.location.origin}>
     <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Route path="/" component={InputReferences} />
       </div>
     </Router>
+  </Auth0Provider>
   );
 }
 
